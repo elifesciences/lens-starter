afc333efa60c19d240fc6e0063ce5bccb6dc9ddc
Casey Ydenberg <ydenberg@gmail.com>
2015-11-11 13:11:27 -0700

    Added browser-sync and nodemon for development

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserify = require('browserify');
@@ -7,20 +7,27 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var through2 = require('through2');
 var path = require('path');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('assets', function() {
+
   gulp.src('assets/**/*', {base:"./assets"})
-        .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream())
 
   gulp.src('data/**/*', {base:"."})
-        .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+
 });
 
 gulp.task('sass', function () {
   gulp.src('./lens.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(rename('lens.css'))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('browserify', function () {
@@ -39,7 +46,29 @@ gulp.task('browserify', function () {
         })
         .pipe(uglify())
         .pipe(rename('lens.js'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['assets', 'sass', 'browserify']);
+
+gulp.task('nodemon', function (cb) {
+	return nodemon({
+		// nodemon our server
+		script: 'server.js',
+	}).once('start', cb);
+});
+
+gulp.task('watch', ['nodemon'], function () {
+	// for more browser-sync config options: http://www.browsersync.io/docs/options/
+	browserSync.init({
+		// informs browser-sync to proxy our expressjs app which would run at the following location
+		proxy: 'http://localhost:4001',
+		// informs browser-sync to use the following port for the proxied app
+		// notice that the default port is 3000, which would clash with our expressjs
+		port: 8000
+	});
+	gulp.watch(['assets/**/*.js'], ['assets']);
+	gulp.watch(['lens.scss'], ['sass']);
+	gulp.watch(['src/**/*.js'], ['browserify']);
+});
